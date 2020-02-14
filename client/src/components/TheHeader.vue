acc092de70c6f20e1cdf8ca4a50ef88e81d8a7cb
mbru78 <mbru@sodifrance.fr>
2020-02-14 10:52:35 +0100

    second commit

--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Header</div>
+  <div class="container-header d-flex w-100 flex-row justify-content-center align-items-center p-3">
+    <router-link to="/">The App Header</router-link>
+    <div class="flex-fill"></div>
+    <div class="d-flex flex-row">
+      <router-link to="/signin" class="mr-2">Connexion</router-link>
+      <router-link to="/signup">Inscription</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,4 +17,13 @@ export default {
 </script>
 
 <style scoped>
+.container-header {
+  background-color: #192a56;
+  color: white;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
 </style>
---
+++ client/src/store.js
@@ -0,0 +1,92 @@
+import Vuex from "vuex";
+import Vue from "vue";
+import axios from "./http";
+import router from "./routes";
+
+Vue.use(Vuex);
+
+const user = {
+    namespaced: true,
+    state: {
+        data: {},
+        isLoading: false,
+        isLoggedIn: null,
+        jwtToken: null,
+        errors: []
+    },
+    getters: {
+        isLoading: state => state.isLoading,
+        isLoggedIn: state => state.isLoggedIn,
+        errors: state => state.errors,
+        currentUser: state => state.data,
+        jwtToken: state => state.jwtToken
+    },
+    actions: {
+        async trySignin(context, credentials) {
+            try {
+                context.commit("updateIsLoading", true);
+                const response = await axios.post("/api/auth", credentials);
+                context.commit("signinSuccess", response.data);
+                router.push("/profile");
+            } catch (err) {
+                context.commit("signError", err);
+            }
+        },
+        async trySignup(context, user) {
+            try {
+                context.commit("updateIsLoading", true);
+                await axios.post("/api/user", user);
+                context.commit("signupSuccess");
+                router.push("/signin");
+            } catch (err) {
+                context.commit("signError", err);
+            }
+        },
+        async fetchCurrentUser(context) {
+            try {
+                context.commit("updateIsLoading", true);
+                const response = await axios.get("/api/user/current");
+                context.commit("fetchCurrentUserSuccess", response.data);
+            } catch (err) {
+                context.commit("signError", err);
+            }
+        }
+    },
+    mutations: {
+        updateIsLoading(state, isLoading) {
+            state.isLoading = isLoading;
+        },
+        signupSuccess(state) {
+            state.isLoading = false;
+            state.errors = [];
+        },
+        signError(state, errors) {
+            state.isLoading = false;
+            state.errors = errors.response.data;
+        },
+        signinSuccess(state, data) {
+            state.isLoading = false;
+            state.errors = [];
+            state.isLoggedIn = true;
+            state.data = data.user;
+            state.jwtToken = data.jwtToken;
+        },
+        signOut(state) {
+            state.jwtToken = null;
+        },
+        fetchCurrentUserSuccess(state, user) {
+            state.data = user;
+            state.isLoading = false;
+            state.isLoggedIn = true;
+            state.errors = [];
+        }
+    }
+};
+
+const store = new Vuex.Store({
+    modules: {
+        user
+    }
+});
+
+export default store;
